0b3a700c4b331dc4319cdfcd4c7a64e79f8c3ef5
mjavier <michaeljaviermota@gmail.com>
2018-07-16 12:03:20 -0400

    Editada

--- PracticaJava/src/Ejecutable.java
+++ PracticaJava/src/Ejecutable.java
@@ -1,74 +1,112 @@
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Ejecutable {
 
+	private static String rrts[];
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				
+		
+		
 		try {
+			ArrayList<Pelicula> peliArra = new ArrayList<Pelicula>();
+			ArrayList<Serie> serieArra = new ArrayList<Serie>();
 			
-					  ArrayList<Pelicula> peliArra = new ArrayList<Pelicula>();
-					  ArrayList<Serie> serieArra = new ArrayList<Serie>();
-								
-								
-					  peliArra.add(new Pelicula());
-					  peliArra.get(0).setTitulo("Pelicula1");
-					  peliArra.get(0).setGenero("Genero1");
-					  peliArra.get(0).setCreador("Creador1");
-					  peliArra.get(0).setAño(2000);	  
-					  LocalTime tim = LocalTime.of(1, 30, 2);
-					  peliArra.get(0).setDuracion(tim);
-					  peliArra.get(0).marcarVisto(true);
-					 
-					  peliArra.add(new Pelicula("Titulo2", "Genero2"));
-					  peliArra.get(1).setCreador("Creador2");
-					  peliArra.get(1).setAño(2002);	  
-					  LocalTime tim2 = LocalTime.of(0, 30, 2);
-					  peliArra.get(1).setDuracion(tim2);
-					  peliArra.get(1).marcarVisto(false);
-					 
-					  
-					  serieArra.add( new Serie());
-					  serieArra.get(0).setTitulo("Serie");
-					  serieArra.get(0).setGenero("Genero1");
-					  serieArra.get(0).setCreador("Creador1");
-					  serieArra.get(0).setAño(2005);	  
-					  LocalTime timeseri = LocalTime.of(0, 40, 2);
-					  serieArra.get(0).setDuracion(timeseri);
-					  serieArra.get(0).marcarVisto(true);
-					  
-					  
-					  serieArra.add(new Serie());
-					  serieArra.get(1).setTitulo("Serie2");
-					  serieArra.get(1).setGenero("Genero2");
-					  serieArra.get(1).setCreador("Creador2");
-					  serieArra.get(1).setAño(2006);	  
-					  LocalTime timeseri2 = LocalTime.of(0, 20, 2);
-					  serieArra.get(1).setDuracion(timeseri2);
-					  serieArra.get(1).marcarVisto(false);
-					 
-					  
-					  for (Serie serie : serieArra) {
-								 
-								System.out.println("\n"+serie.toString());
-					
-					}
-					 
-					  for (Pelicula peli : peliArra) {
-								
-								System.out.println("\n"+peli.toString());
-					
-					}
+
+			
+  peliArra.add(new Pelicula());
+peliArra.get(0).setTitulo("Pelicula1");
+peliArra.get(0).setGenero("Genero1");
+peliArra.get(0).setCreador("Creador1");
+peliArra.get(0).setAño(2001);	  
+  LocalTime tim = LocalTime.of(1, 40, 0);
+peliArra.get(0).setDuracion(tim);
+  LocalTime tiempoVisto = LocalTime.of(1, 40, 0);
+peliArra.get(0).setTiempoVisto(tiempoVisto);
+peliArra.get(0).marcarVisto();
+ 
+peliArra.add( new Pelicula("Pelicula2", "Genero2"));	  
+peliArra.add( new Pelicula("Pelicula3","Genero3","Creador3",2003,LocalTime.of(1, 40, 0),LocalTime.of(0, 10, 2))); 
+peliArra.add( new Pelicula("Pelicula4","Genero4","Creador4",2004,LocalTime.of(1, 10, 0),LocalTime.of(0, 5, 2)));
+peliArra.add( new Pelicula("Pelicula5","Genero5","Creador5",2005,LocalTime.of(1, 40, 0),LocalTime.of(1, 40, 0)));
+  peliArra.get(4).marcarVisto();
+  
+  serieArra.add(new Serie());
+  serieArra.get(0).setTitulo("Serie1");
+  serieArra.get(0).setGenero("Genero1");
+  serieArra.get(0).setCreador("Creador1");
+  serieArra.get(0).setAño(1991);	  
+  LocalTime timeseri = LocalTime.of(0, 40, 0);
+  serieArra.get(0).setDuracion(timeseri);
+  LocalTime SerieTiempoVisto = LocalTime.of(0, 40, 0);
+  serieArra.get(0).setTiempoVisto(SerieTiempoVisto);
+  serieArra.get(0).marcarVisto();
+  
+  
+  serieArra.add( new Serie("Serie2", "Genero2"));	  
+  serieArra.add(new Serie("Serie3","Genero3","Creador3",3,1992,LocalTime.of(0, 40, 0),LocalTime.of(0, 6, 0)));	 
+  serieArra.add( new Serie("Serie4","Genero4","Creador4",4,1993,LocalTime.of(0, 40, 0),LocalTime.of(0, 14, 0)));
+  serieArra.add( new Serie("Serie5","Genero5","Creador5",5,1994,LocalTime.of(0, 40, 0),LocalTime.of(0, 40, 0)));
+  serieArra.get(4).marcarVisto();
+  
+  
+  /*General*/
+  System.out.println("\n *****************************IMPRESION DE PELICULAS Y SERIES EN GENERAL");
+  for (Serie serie : serieArra) {
+			  if(serie != null)
+			System.out.println(serie.getTitulo()+":"+((serie.toString().replace('[', ' ').replace(']', ' '))));
+
+}
+ 
+  for (Pelicula peli : peliArra) {
+			  if(peli != null)
+//	
+			  System.out.println(peli.getTitulo()+":"+((peli.toString().replace('[', ' ').replace(']', ' '))));
+
+}
+  
+  /*Peliculas y Series que se visualizaron completamente*/
+  System.out.println("\n *****************************PELICULAS Y SERIES QUE SE VIZUALIZARON COMPLETAMENTE");
+  for (Serie serie : serieArra) {
+			  if(serie.visto == true) {
+//	
+			System.out.println(serie.getTitulo()+":"+((serie.toString().replace('[', ' ').replace(']', ' '))));
+			
+			  }
+
+}
+ 
+				  for (Pelicula peli : peliArra) {
+							  if(peli.visto == true) {
+							//
+							  System.out.println(peli.getTitulo()+":"+((peli.toString().replace('[', ' ').replace(']', ' '))));
+				
+							  	} 
+				  }
+			  
+			  /*Serie con mas temporadas y pelicula del anio mas reciente*/
+			  System.out.println("\n *****************************SERIE CON MAS TEMPORADAS Y PELICULA DEL ANIO MAS RECIENTE");
+			  Collections.sort(serieArra, new Serie());
+			  
+			
+			  System.out.println("Serie con mas temporada: "+serieArra.get(0));
+				  
+			   Collections.sort(peliArra, new Pelicula());
+				  System.out.println("Pelicula del anio mas reciente: "+peliArra.get(0));
+				  
+				  
+				 
+				  
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	  
 	  
-	  
-	  
 	 
 	}
 
 }
+
--- PracticaJava/src/Serie.java
+++ PracticaJava/src/Serie.java
@@ -1,7 +1,10 @@
 
 import java.time.LocalTime;
+import java.util.Comparator;
 
-public class Serie extends Pelicula {
+
+
+public class Serie extends Video implements IVisualizable,Comparator<Serie>  {
 
 
 	private int no_temporada;
@@ -11,16 +14,30 @@ public class Serie extends Pelicula {
 	public Serie() {
 		
 		this.no_temporada =1;
+	    super.visto =false;
+	}
 	
+	public Serie(String titulo, String creador) {
+
+		super(titulo,creador);
+	
+		}
+	
+	
+	public Serie(String titulo, String genero, String creador,int no_temporada, int año, LocalTime duracion,LocalTime tiempoVisto) {
+		super( titulo,  genero,  creador,  año,  duracion,tiempoVisto);
+		  super.visto =false;
+		  this.no_temporada=no_temporada;
+		
 	}
 	
 	
-	public Serie(int no_temporada, String genero, LocalTime duracion) {
-		super("Creado por defecto",genero, "Michael Javier", no_temporada, duracion,false);
+	/*public Serie(int no_temporada, String genero, LocalTime duracion) {
+	//	super("Creado por defecto",genero, "Michael Javier", no_temporada, duracion,false);
 		this.no_temporada = no_temporada;
 			
 	
-	}
+	}*/
 
 	
 	public int getNo_temporada() {
@@ -35,18 +52,47 @@ public class Serie extends Pelicula {
 	@Override
 	public String toString() {
 		
-		String resul ="Serie \n "
-				+ "titulo=" + getTitulo() 
+		String resul =""
+				+ "  titulo=" + getTitulo() 
 				+ ", genero=" + getGenero() 
 				+ ", creador=" + getCreador()
-				+ ", año=" + getAño()
+				+ ", año=" + getAnio()
 				+ ", duracion=" + getDuracion()
+				+ ", tiempoVisto=" + getTiempoVisto()
 				+ ", visto=" + esVisto()
-				+"  no_temporada=" + getNo_temporada()
-		+ ", tiempoVisto()=" + tiempoVisto();
+				+"  no_temporada=" + getNo_temporada() +"";
 		return resul;
 		
 		
+	}
+
+	@Override
+	public void marcarVisto() {
+		// TODO Auto-generated method stub
+		
+        super.visto = true;
+		
+	}
+
+	@Override
+	public boolean esVisto() {
+		// TODO Auto-generated method stub
+		return super.visto;
+	}
+
+	@Override
+	public LocalTime tiempoVisto() {
+		// TODO Auto-generated method stub
+		return getTiempoVisto();
+	}
+
+
+
+	@Override
+	public int compare(Serie arg0, Serie arg1) {
+		// TODO Auto-generated method stub
+		 //return arg0.no_temporada - arg1.no_temporada;
+		 return  arg1.no_temporada-arg0.no_temporada ;
 	}
 
 
